d1a907cde9c8880e9223e36dea3fe2fd78593cc2
Yogesh Yadav <yogeshyadavyy8736659@gmail.com>
2026-05-20 17:20:18 +0530

    Bubble sort on list

--- practice.py
+++ practice.py
@@ -1,3 +1,21 @@
-print('Hello, World!')
-print('Welcome to Python programming.')
-print('testing ')
+# print('Hello, World!')
+# print('Welcome to Python programming.')
+# print('testing ')
+
+list = [8,10,6,2,4]
+swapped = True
+count = 0
+while swapped:
+    swapped = False
+    for i in range(len(list)-1):
+        count+=1
+        if list[i] > list[i+1]:
+            swapped = True
+            list[i], list[i+1] = list[i+1], list[i]
+print('Sorted list:', list)
+print('Number of comparisons:', count)
+
+
+list = [8,10,6,3,6,7,2,4]
+list.sort()
+print('Sorted list:', list)
