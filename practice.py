d9b0a1c5f820e1d6924044303c7dd4fee1f9f1ca
Yogesh Yadav <yogeshyadavyy8736659@gmail.com>
2026-05-20 18:03:33 +0530

    list functions

--- practice.py
+++ practice.py
@@ -2,54 +2,55 @@
 # print('Welcome to Python programming.')
 # print('testing ')
 
-list = [8,10,6,2,4]
-swapped = True
-count = 0
-while swapped:
-    swapped = False
-    for i in range(len(list)-1):
-        count+=1
-        if list[i] > list[i+1]:
-            swapped = True
-            list[i], list[i+1] = list[i+1], list[i]
-print('Sorted list:', list)
-print('Number of comparisons:', count)
+# list = [8,10,6,2,4]
+# swapped = True
+# count = 0
+# while swapped:
+#     swapped = False
+#     for i in range(len(list)-1):
+#         count+=1
+#         if list[i] > list[i+1]:
+#             swapped = True
+#             list[i], list[i+1] = list[i+1], list[i]
+# print('Sorted list:', list)
+# print('Number of comparisons:', count)
 
 
-list = [8,10,6,3,6,7,2,4]
-list.sort()
-list.reverse()
-print('Sorted list:', list)
-print('reverse sorted list:', list)
+# list = [8,10,6,3,6,7,2,4]
+# list.sort()
+# list.reverse()
+# print('Sorted list:', list)
+# print('reverse sorted list:', list)
 
 
-list_1 = [1]
-list_2 = list_1[:]
-list_1[0] = 2
-print('list_1:', list_1)
-print('list_2:', list_2)
+# list_1 = [1]
+# list_2 = list_1[:]
+# list_1[0] = 2
+# print('list_1:', list_1)
+# print('list_2:', list_2)
 
 
-my_list = [10,8,6,4,2]
-new_list = my_list[1:3]
-print(new_list)
+# my_list = [10,8,6,4,2]
+# new_list = my_list[1:3]
+# print(new_list)
 
 
-my_list = [10,8,6,4,2]
-new_list = my_list[-5:3]
-print(new_list)
+# my_list = [10,8,6,4,2]
+# new_list = my_list[-5:3]
+# print(new_list)
 
-new_list = my_list[:3]
-print(new_list)
-
-
-new_list = my_list[2:]
-print(new_list)
+# new_list = my_list[:3]
+# print(new_list)
 
 
-list = [10,8,6,4,2]
-del list[1 :3]
-print(list)
+# new_list = my_list[2:]
+# print(new_list)
 
-del list[:]
-print(list)
+
+# list = [10,8,6,4,2]
+# del list[1 :3]
+# print(list)
+
+# del list[:]
+# print(list)
+
