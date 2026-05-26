de9c604c93f928e88602157621c2cff6a3cabe16
Yogesh Yadav <yogeshyadavyy8736659@gmail.com>
2026-05-26 18:03:57 +0530

    Tuple started

--- practice.py
+++ practice.py
@@ -418,4 +418,15 @@
     
 # print(factorial(5))
 
+my_tuple = (1,10,100)
 
+t1 = my_tuple+ (1000,10000)
+t2 = my_tuple*3
+
+print(len(my_tuple))
+print(len(t1))
+print(len(t2))
+print(t1)
+print(t2)
+print(10 in my_tuple)
+print(10 not in my_tuple)
