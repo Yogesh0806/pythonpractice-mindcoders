1761b93a4b9912d35743cb0c1c15b29476d06069
Yogesh Yadav <yogeshyadavyy8736659@gmail.com>
2026-05-27 17:00:47 +0530

    Tuple completed

--- practice.py
+++ practice.py
@@ -418,15 +418,75 @@
     
 # print(factorial(5))
 
-my_tuple = (1,10,100)
+# my_tuple = (1,10,100)
 
-t1 = my_tuple+ (1000,10000)
-t2 = my_tuple*3
+# t1 = my_tuple+ (1000,10000)
+# t2 = my_tuple*3
 
-print(len(my_tuple))
-print(len(t1))
-print(len(t2))
-print(t1)
-print(t2)
-print(10 in my_tuple)
-print(10 not in my_tuple)
+# print(len(my_tuple))
+# print(len(t1))
+# print(len(t2))
+# print(t1)
+# print(t2)
+# print(10 in my_tuple)
+# print(10 not in my_tuple)
+
+# my_tuple = (1,10,100)
+# print(len(my_tuple))
+# my_tuple +=(1000,10000)
+# print(my_tuple)
+# print(len(my_tuple))
+
+# tuple_1 = (1,2,3)
+# for element in tuple_1:
+#     print(element)
+
+# tuple_2 = (1,2,3,4)
+# print(tuple_2)
+# print(5 in tuple_2)
+# print(10 in tuple_2)
+# print(7 not in tuple_2)
+# print(2 in tuple_2)
+
+
+# tuple_3 = (1,2,3,4)
+# print(len(tuple_3))
+# print(5 not in tuple_3)
+
+# tuple_4 = tuple_1 + tuple_2
+# tuple_5 = tuple_3 * 2
+# print(tuple_4)
+# print(tuple_5)
+
+
+# my_tuple = tuple((1,2,'string'))
+# print(my_tuple)
+# print(type(my_tuple))
+
+# my_list = [2,4,6]
+# print(my_list)
+# print(type(my_list))
+# tup = tuple(my_list)
+# print(tup)
+# print(type(tup))
+
+
+# my_tuple = (1,2,3)
+# list =[]
+# for element in my_tuple:
+#     list.append(element*2)
+    
+# print(tuple(list))
+
+
+# var =123
+
+# t1 =(1, )
+# t2 =(2, )
+# t3 =(3, )
+
+# t1, t2, t3 = t2, t3, t1
+
+# print(t1, t2, t3)
+# print(type(t1), type(t2), type(t3))
+
