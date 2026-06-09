1ff899e4b62065ce153dfb0543091e19a581121d
Yogesh Yadav <yogeshyadavyy8736659@gmail.com>
2026-06-09 17:27:28 +0530

    Exceptions in oop

--- practice.py
+++ practice.py
@@ -1006,22 +1006,160 @@
 
 # Name mangling is a technique used in Python to access private attributes and methods of a class. When an attribute or method is defined with double underscores (e.g., __attribute), it is considered private and cannot be accessed directly from outside the class. However, Python provides a way to access these private members using name mangling, which involves prefixing the attribute or method name with the class name and a single underscore (e.g., _ClassName__attribute). This allows us to access the private members indirectly while still maintaining encapsulation.
 
-class Classy:
-    def visible(self):
-        print('This method is visible')  
+# class Classy:
+#     def visible(self):
+#         print('This method is visible')  
     
-    def __hidden(self):
-        print('This method is hidden')
+#     def __hidden(self):
+#         print('This method is hidden')
         
-obj = Classy()
-obj.visible() #output: visible
-try:
-    obj.__hidden() # This fails
-except:
-    print('Failed to access hidden method') #output: Failed to access hidden method
+# obj = Classy()
+# obj.visible() #output: visible
+# try:
+#     obj.__hidden() # This fails
+# except:
+#     print('Failed to access hidden method') #output: Failed to access hidden method
     
-obj._Classy__hidden() #output: This method is hidden
+# obj._Classy__hidden() #output: This method is hidden
 
-obj = Classy()
-print(type(obj))
-print(type(obj).__name__)
+# obj = Classy()
+# print(type(obj))
+# print(type(obj).__name__)
+
+#INHERITANCE CONTINUE
+
+
+# class Vehicle:
+#     pass 
+
+# class LandVehicle(Vehicle):
+#     pass
+
+# class TrackedVehicle(LandVehicle):
+#     pass
+
+# my_vehicle = Vehicle()
+# my_land_vehicle = LandVehicle()
+# my_tracked_vehicle = TrackedVehicle()
+
+
+# for obj in [my_vehicle, my_land_vehicle, my_tracked_vehicle]:
+#     for cls in [Vehicle, LandVehicle, TrackedVehicle]:
+#         print(isinstance(obj, cls), end='\t') #isinstance() is a built-in function in Python that checks if an object is an instance of a specified class or a subclass thereof. It takes two arguments: the first argument is the object to check, and the second argument is the class or a tuple of classes to compare against. The function returns True if the object is an instance of the specified class or any of its subclasses, and False otherwise.
+#     print()
+    
+# class SampleClass:
+#     def __init__(self,val):
+#         self.val = val
+        
+# object_1 = SampleClass(0)
+# object_2 = SampleClass(2)
+# object_3 = object_1
+# object_3.val += 1
+
+# print(object_1 is object_2)
+# print(object_2 is object_3)
+# print(object_1 is object_3)
+# print(object_3 is object_1)
+# print(object_1.val,object_2.val,object_3.val)
+
+
+# string_1 = 'Mary had a little '
+# string_2 = 'Mary had a little lamb'
+# string_1 += 'lamb' 
+
+# print(string_1 == string_2, string_1 is string_2)
+
+
+# class Super :
+#     def __init__(self,name):
+#         self.name = name
+        
+#     def __str__(self):
+#         return 'My name is ' + self.name+ '.'
+    
+# class Sub(Super):
+#     def __init__(self,name):
+#         super().__init__(name)
+        
+# obj = Sub('Andy')
+# print(obj)
+
+# class SuperA:
+#     var_a =10
+#     def fun_a(self):
+#         return 11
+    
+# class SuperB:
+#     var_b =20
+#     def fun_b(self):
+#         return 21
+    
+# class Sub(SuperA, SuperB):
+#     pass
+
+# obj = Sub()
+# print(obj.var_a, obj.fun_a())
+# print(obj.var_b, obj.fun_b())
+
+#MULTIPLE INHERITANCE 
+
+# class Level1:
+#     var = 100
+#     def fun(self):
+#         return 101
+    
+# class Level2:
+#     var = 200
+#     def fun(self): #Overriding the fun method of Level1 class in Level2 class
+#         return 201 #overrides Level1.fun()
+
+# class Level3(Level2):
+#     pass
+
+# obj = Level3()
+# print(obj.var, obj.fun())
+
+#MULTIPLE INHERITANCE 
+
+
+
+# class Left:
+#     var = 'L'
+#     var_left = 'LL'
+#     def fun(self):
+#         return 'Left'
+
+# class Right:
+#     var = 'R'
+#     var_right = 'RR'
+#     def fun(self):
+#         return 'Right'
+    
+# class Sub(Left, Right):
+#     pass
+
+# obj = Sub()
+# print(obj.var,obj.var_left, obj.var_right, obj.fun())
+
+#polymorphism
+
+
+class One:
+    def do_it(self):
+        print('Do_it from one')
+    
+    def doAnything(self):
+        self.do_it() #This line calls the do_it() method of the class. However, since the do_it() method is overridden in the Two class, when doAnything() is called on an instance of Two, it will use the overridden version of do_it() from the Two class instead of the one from the One class. This is an example of polymorphism, where a method can have different implementations in different classes, and the appropriate implementation is chosen at runtime based on the type of the object calling the method.
+    
+class Two(One):
+    def do_it(self):
+        print('Do_it from two')
+    
+    
+one = One()
+two = Two()
+one.doAnything() #Output: Doing anything in One way
+two.doAnything() #Output: Doing anything in Two way because of polymorphism, where the method do_it() is overridden in the Two class, and when doAnything() is called on an instance of Two, it uses the overridden version of do_it() from the Two class instead of the one from the One class.
+
+
